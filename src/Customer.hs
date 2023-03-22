cachix/customerio-client

Add initial haskell project

src/Customer.hs
@@ -1,0 +1,62 @@
+-- SPDX-FileCopyrightText: 2022 Serokell <https://serokell.io/>
+--
+-- SPDX-License-Identifier: Apache-2.0
+
+{-# LANGUAGE RecordWildCards #-}
+
+module Customer
+  ( module Customer
+  , module Customer.Track.Events.Types.ReportPushMetrics
+  , module Customer.Track.Events.Types.TrackAnonymusEvent
+  , module Customer.Track.Events.Types.TrackCustomerEvent
+  ) where
+
+import Customer.Track.Events.API (api)
+import Customer.Track.Events.Types.ReportPushMetrics
+import Customer.Track.Events.Types.TrackAnonymusEvent
+import Customer.Track.Events.Types.TrackCustomerEvent
+import Data.Text (Text)
+import qualified Network.HTTP.Client as HTTP
+import Servant.API
+import Servant.Client
+
+-- | Default host of the track API: https://track.customer.io:443
+host :: BaseUrl
+host = BaseUrl Https "track.customer.io" 443 ""
+
+data Env = MkEnv
+  { authtoken :: BasicAuthData
+  , clientEnv :: ClientEnv
+  }
+
+-- | Same as `mkEnvDef`, but you can change BaseUrl.
+--   May be useful only if `host` is outdated.
+mkEnv :: BaseUrl -> BasicAuthData -> HTTP.Manager -> Env
+mkEnv host' authtoken httpManager = MkEnv {..}
+  where
+    clientEnv = mkClientEnv httpManager host'
+
+-- | Default way to create client environment
+mkEnvDef :: BasicAuthData -> HTTP.Manager -> Env
+mkEnvDef = mkEnv host
+
+trackCustomerEventC :: BasicAuthData -> Text -> TrackCustomerEventBody -> ClientM ()
+trackAnonymusEventC :: BasicAuthData -> TrackAnonymusEventBody -> ClientM ()
+reportPushMetricsC  :: ReportPushMetricsBody -> ClientM ()
+
+trackCustomerEventC
+  :<|> trackAnonymusEventC
+  :<|> reportPushMetricsC
+  = client api
+
+trackCustomerEvent :: Env -> Text -> TrackCustomerEventBody -> IO (Either ClientError ())
+trackCustomerEvent MkEnv{..} identifier body = do
+  runClientM (trackCustomerEventC authtoken identifier body) clientEnv
+
+trackAnonymusEvent :: Env -> TrackAnonymusEventBody -> IO (Either ClientError ())
+trackAnonymusEvent MkEnv{..} body = do
+  runClientM (trackAnonymusEventC authtoken body) clientEnv
+
+reportPushMetrics :: Env -> ReportPushMetricsBody -> IO (Either ClientError ())
+reportPushMetrics MkEnv{..} body = do
+  runClientM (reportPushMetricsC body) clientEnv

src/Customer/Track/Events/API.hs
@@ -1,0 +1,44 @@
+-- SPDX-FileCopyrightText: 2022 Serokell <https://serokell.io/>
+--
+-- SPDX-License-Identifier: Apache-2.0
+
+module Customer.Track.Events.API (module Customer.Track.Events.API) where
+
+import Customer.Track.Events.Types.ReportPushMetrics (ReportPushMetricsBody)
+import Customer.Track.Events.Types.TrackAnonymusEvent (TrackAnonymusEventBody)
+import Customer.Track.Events.Types.TrackCustomerEvent (TrackCustomerEventBody)
+import Data.Proxy (Proxy(..))
+import Data.Text (Text)
+import Servant.API (BasicAuth, Capture, JSON, Post, ReqBody, type (:<|>), type (:>))
+
+type BasicAuthToken = BasicAuth "API Token" Text
+
+type TrackCustomerEvent
+  =  BasicAuthToken
+  :> Capture "identifier" Text
+  :> "events"
+  :> ReqBody '[JSON] TrackCustomerEventBody
+  :> Post '[JSON] ()
+
+type TrackAnonymusEvent
+  =  BasicAuthToken
+  :> "events"
+  :> ReqBody '[JSON] TrackAnonymusEventBody
+  :> Post '[JSON] ()
+
+type ReportPushMetrics
+  = "push"
+  :> "events"
+  :> ReqBody '[JSON] ReportPushMetricsBody
+  :> Post '[JSON] ()
+
+type API
+  = "api"
+  :> "v1"
+  :> ( TrackCustomerEvent
+  :<|> TrackAnonymusEvent
+  :<|> ReportPushMetrics
+  )
+
+api :: Proxy API
+api = Proxy

serokell/customer-client

src/Customer.hs
@@ -1,0 +1,62 @@
+-- SPDX-FileCopyrightText: 2022 Serokell <https://serokell.io/>
+--
+-- SPDX-License-Identifier: Apache-2.0
+
+{-# LANGUAGE RecordWildCards #-}
+
+module Customer
+  ( module Customer
+  , module Customer.Track.Events.Types.ReportPushMetrics
+  , module Customer.Track.Events.Types.TrackAnonymousEvent
+  , module Customer.Track.Events.Types.TrackCustomerEvent
+  ) where
+
+import Customer.Track.Events.API (api)
+import Customer.Track.Events.Types.ReportPushMetrics
+import Customer.Track.Events.Types.TrackAnonymousEvent
+import Customer.Track.Events.Types.TrackCustomerEvent
+import Data.Text (Text)
+import qualified Network.HTTP.Client as HTTP
+import Servant.API
+import Servant.Client
+
+-- | Default host of the track API: https://track.customer.io:443
+host :: BaseUrl
+host = BaseUrl Https "track.customer.io" 443 ""
+
+data Env = MkEnv
+  { authtoken :: BasicAuthData
+  , clientEnv :: ClientEnv
+  }
+
+-- | Same as `mkEnvDef`, but you can change BaseUrl.
+--   May be useful only if `host` is outdated.
+mkEnv :: BaseUrl -> BasicAuthData -> HTTP.Manager -> Env
+mkEnv host' authtoken httpManager = MkEnv {..}
+  where
+    clientEnv = mkClientEnv httpManager host'
+
+-- | Default way to create client environment
+mkEnvDef :: BasicAuthData -> HTTP.Manager -> Env
+mkEnvDef = mkEnv host
+
+trackCustomerEventC :: BasicAuthData -> Text -> TrackCustomerEventBody -> ClientM ()
+trackAnonymousEventC :: BasicAuthData -> TrackAnonymousEventBody -> ClientM ()
+reportPushMetricsC  :: ReportPushMetricsBody -> ClientM ()
+
+trackCustomerEventC
+  :<|> trackAnonymousEventC
+  :<|> reportPushMetricsC
+  = client api
+
+trackCustomerEvent :: Env -> Text -> TrackCustomerEventBody -> IO (Either ClientError ())
+trackCustomerEvent MkEnv{..} identifier body = do
+  runClientM (trackCustomerEventC authtoken identifier body) clientEnv
+
+trackAnonymousEvent :: Env -> TrackAnonymousEventBody -> IO (Either ClientError ())
+trackAnonymousEvent MkEnv{..} body = do
+  runClientM (trackAnonymousEventC authtoken body) clientEnv
+
+reportPushMetrics :: Env -> ReportPushMetricsBody -> IO (Either ClientError ())
+reportPushMetrics MkEnv{..} body = do
+  runClientM (reportPushMetricsC body) clientEnv

src/Customer/Track/Events/API.hs
@@ -1,0 +1,44 @@
+-- SPDX-FileCopyrightText: 2022 Serokell <https://serokell.io/>
+--
+-- SPDX-License-Identifier: Apache-2.0
+
+module Customer.Track.Events.API (module Customer.Track.Events.API) where
+
+import Customer.Track.Events.Types.ReportPushMetrics (ReportPushMetricsBody)
+import Customer.Track.Events.Types.TrackAnonymousEvent (TrackAnonymousEventBody)
+import Customer.Track.Events.Types.TrackCustomerEvent (TrackCustomerEventBody)
+import Data.Proxy (Proxy(..))
+import Data.Text (Text)
+import Servant.API (BasicAuth, Capture, JSON, Post, ReqBody, type (:<|>), type (:>))
+
+type BasicAuthToken = BasicAuth "API Token" Text
+
+type TrackCustomerEvent
+  =  BasicAuthToken
+  :> Capture "identifier" Text
+  :> "events"
+  :> ReqBody '[JSON] TrackCustomerEventBody
+  :> Post '[JSON] ()
+
+type TrackAnonymousEvent
+  =  BasicAuthToken
+  :> "events"
+  :> ReqBody '[JSON] TrackAnonymousEventBody
+  :> Post '[JSON] ()
+
+type ReportPushMetrics
+  = "push"
+  :> "events"
+  :> ReqBody '[JSON] ReportPushMetricsBody
+  :> Post '[JSON] ()
+
+type API
+  = "api"
+  :> "v1"
+  :> ( TrackCustomerEvent
+  :<|> TrackAnonymousEvent
+  :<|> ReportPushMetrics
+  )
+
+api :: Proxy API
+api = Proxy